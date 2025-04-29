6bc0c385678b121ac7d67d05ac7ece4fe042d102
SaraBarkat <ms_barkat@esi.dz>
2025-04-29 22:32:12 +0100

    update

--- Backend/blogify/blogify/settings.py
+++ Backend/blogify/blogify/settings.py
@@ -74,12 +74,21 @@ WSGI_APPLICATION = 'blogify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
+
+SECRET_KEY = config('SECRET_KEY')
+
 
 
 # Password validation
